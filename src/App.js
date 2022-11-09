1b721dc832c9c44c15ab43e304db032e9374a157
janardhan5031 <janardhan5031>
2022-11-09 12:39:31 +0530

    adding expense through react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from './componenents/ExpenseItems'
+
+function App() {
+  return (
+    <div>
+      HELLO THERE hfddhfh
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componenents/ExpenseItems.js
@@ -0,0 +1,21 @@
+
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <h2>food</h2>
+                <h4>RS 10</h4>
+            </div>
+            <div>
+                <h2>petrol</h2>
+                <h4>RS 100</h4>
+            </div>
+            <div>
+                <h2>movie</h2>
+                <h4>RS 200</h4>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
