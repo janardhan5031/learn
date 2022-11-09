47823e7e05d5a6919ae13b9a15bb497c7841dafd
janardhan5031 <janardhan5031>
2022-11-09 18:38:40 +0530

    adding css to date component

--- src/componenents/ExpenseItems.js
+++ src/componenents/ExpenseItems.js
@@ -6,10 +6,10 @@ function ExpenseItem(prop) {
     return (
         <div>
             <div className='expense-item'>
-                <ExpenseDate date={ prop.obj.date}></ExpenseDate>
+                <ExpenseDate date={ prop.obj.date}/>
                 <div className='expense-item__description'>
                     <h2>{ prop.obj.desc}</h2>
-                    <div className='expense-item__pice'>{ prop.obj.price}</div>
+                    <div className='expense-item__price'>{ prop.obj.price}</div>
                 </div>
             </div>
         </div>
